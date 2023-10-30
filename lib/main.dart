ccf907ff1f4a74ef1d5f5b3fc8c4f31f2f2e0f7d
adarshhhr <adarshramesh2002@gmail.com>
2023-10-30 14:00:45 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -146,33 +146,135 @@ class MyApp extends StatelessWidget {
                       padding: const EdgeInsets.all(8),
                       scrollDirection: Axis.vertical,
                       children: <Widget>[
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                          child: Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                width: 177, // Set the desired width
-                                height: 128, // Set the desired height
-                                child: Column(
-                                  children: [
-                                    Align(
-                                      alignment: Alignment
-                                          .center, // Align to the center
-                                      child: Image.asset(
-                                        'images/Cabbage_and_cross_section_on_white.jpg',
-                                        width: 177, // Set the image width
-                                        height: 128, // Set the image height
+                        Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.purpleAccent, width: 0),
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              width: 177, // Set the desired width
+                              height: 128, // Set the desired height
+                              child: Column(
+                                children: [
+                                  Align(
+                                    alignment: Alignment.center,
+                                    child: Image.asset(
+                                      'images/Boston.png',
+                                      width: 177,
+                                      height: 128,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white.withOpacity(0.1),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              width: 177,
+                              height: 128,
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        12,
+                                        8,
+                                        12,
+                                        8), // Left, Top, Right, Bottom padding
+                                    child: Container(
+                                        child: const Text(
+                                      'Boston Lettuce',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.indigo,
+                                          fontSize: 18),
+                                    )),
+                                  ),
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.fromLTRB(26, 0, 12, 8),
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                            child: const Text(
+                                          '1.10',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.indigo,
+                                              fontSize: 18),
+                                        )),
+                                        Container(
+                                            child: const Text(
+                                          '\$/piece',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.grey,
+                                              fontSize: 15),
+                                        )),
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    // Child 3
+                                    child: Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          27, 8, 12, 8),
+                                      child: Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                right: 10), // Add spacing here
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                  color: Colors.white,
+                                                  borderRadius:
+                                                      BorderRadius.circular(8),
+                                                  border: Border.all(
+                                                      color: Colors.black,
+                                                      width: 1)),
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      15, 10, 15, 10),
+                                              child: const Icon(
+                                                Icons.favorite,
+                                                color: Colors.red,
+                                                size: 24,
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                right: 10), // Add spacing here
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                  color: Colors.green,
+                                                  borderRadius:
+                                                      BorderRadius.circular(8),
+                                                  border: Border.all(
+                                                      color: Colors.black,
+                                                      width: 1)),
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      15, 10, 15, 10),
+                                              child: const Icon(
+                                                Icons.add_shopping_cart_sharp,
+                                                color: Colors.white,
+                                                size: 24,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
                         ),
                       ],
                     ),
@@ -183,7 +285,7 @@ class MyApp extends StatelessWidget {
           ],
         ),
         bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: const Color.fromRGBO(246, 245, 245, 0.9),
+          backgroundColor: const Color.fromRGBO(246, 245, 245, 0.7),
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Image.asset('images/tab2,3.png'),
